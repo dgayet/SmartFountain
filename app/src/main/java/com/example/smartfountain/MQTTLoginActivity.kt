b97312ade803073d3f5a30968a43a14a11fb90a2
Denise Gayet <dgayet@fi.uba.ar>
2021-12-22 03:55:45 -0300

    First Commit

---
+++ app/src/main/java/com/example/smartfountain/MQTTLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartfountain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MQTTLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mqttlogin)
+    }
+}
